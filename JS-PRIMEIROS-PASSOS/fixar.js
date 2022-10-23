b831aca179e4e9bcb1842ccddf9c7a55577c6ebe
venturdan <venturdan@outlook.com.br>
2022-10-23 20:14:27 -0300

    feito alguns exercicios de fixacao para melhor aprendizado

---
+++ JS-PRIMEIROS-PASSOS/fixar.js
@@ -0,0 +1,10 @@
+const notaMedia = 70;
+if (notaMedia >= 80) {
+    console.log("Parabéns, você foi aprovado!")
+}
+else if (notaMedia <= 80 && notaMedia >= 60) {
+    console.log("Você está na lista e espera!");
+}
+else {
+    console.log("Você foi reprovado");
+}
---
+++ JS-PRIMEIROS-PASSOS/scripts.js
@@ -0,0 +1,13 @@
+const myName = "Daniel A. Ventura";
+const birthCity = "Campina Grande";
+let birthYear = 1998;
+
+console.log(myName, birthCity, birthYear);
+
+const base = '5';
+const height = '8';
+const area = base * height;
+const perimeter = (base*height*area);
+
+console.log(base, height, area);
+console.log(perimeter);
